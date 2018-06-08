c8a4a3e6c41c45f0c78449d6e6a0c79c51eea905
Mahesh Pawar <mahesh.pawar149@gmail.com>
2018-06-08 15:06:38 +0530

    With Comment upload

--- Jira_API.py
+++ Jira_API.py
@@ -14,24 +14,58 @@ def main():
 		#for i in json_data:
 			#print ("%s: %s" % (i,json_data[i]))
 
-
+		
 		print Jira_Tickets[i];
 		statusVal=json_data['fields']['status']['name']
-		print "+++++++++++++++++++++++++++++++++++++++++++++"
+		
 		if (statusVal=="Request In Execution"):
 			print "Request Found";
-			DurationValue=str(json_data['fields']['customfield_10207'])
-			EnvValue=json_data['fields']['customfield_10208']['value']
-			DurValue=json_data['fields']['customfield_10209']['name']
-
+			UserName=str(json_data['fields']['creator']['name']);
+			Usermail=str(json_data['fields']['creator']['emailAddress']);
+			GroupName=str(json_data['fields']['customfield_10209']['name']);
+			RequestType=str(json_data['fields']['issuetype']['name']);
+			DurationValue=str(json_data['fields']['customfield_10207']);
+			EnvValue=str(json_data['fields']['customfield_10208']['value']);
+			
+			print "+++++++++++++++++++++++++++++++++++++++++++++"
+			print UserName;
+			print Usermail;
+			print GroupName;
+			print RequestType;
 			print statusVal;
 			print DurationValue;
-			print EnvValue;
-			print DurValue;
+			print EnvValue;			
+			print "+++++++++++++++++++++++++++++++++++++++++++++"
+			print "CALL 'ENVAllocation_Script.py' HERE with above parameters!!!";
+
+			print "Result of the Script passes to JiraComments";
+			#print "Request Processed for %s,%s,%s,%s,%s,%s,%s"%(UserName,Usermail,GroupName,RequestType,statusVal,DurationValue,EnvValue);
+
+			headers={'Authorization':'Basic YWRtaW46Qm9uZUJvbmUxJA==','content-type' : 'application/json'} #'X-Atlassian-Token': 'nocheck',
+					#,'content-type': 'application/json'}
+			#headers = {'content-type' : 'application/json'}
+			#data={"body": "TEST Comment",}
+			# data=[ {"body": "TEST Comment",
+  	# 			"visibility": {
+  	# 				 "type": "role",
+  	# 				 "value": "Administrators"
+  	# 				 }
+  	# 			}],
+
+			data={"body": "Request Processed for %s,%s,%s,%s,%s,%s,%s"%(UserName,Usermail,GroupName,RequestType,statusVal,DurationValue,EnvValue),
+					"visibility": {
+						"type": "role",
+						"value": "Administrators"
+						}
+					}
+			response = requests.post('http://10.20.2.237:8181/rest/api/2/issue/'+Jira_Tickets[i]+'/comment',headers=headers,data=json.dumps(data)); #auth=auth
+			
+			
+
 		#else:
 			#print "No request Found in Jira Project!!!."
 
-	
+
 main();
 ##***End of File***
 
